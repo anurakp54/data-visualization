c665e9044d04cc73c0e910b097a19372d2310d48
hiddengems <67134102+anurakp54@users.noreply.github.com>
2024-11-06 00:04:05 +0700

    Version 1.0 - Add Time History Plot

--- pages/Tunnel_Convergence.py
+++ pages/Tunnel_Convergence.py
@@ -10,6 +10,14 @@ import pickle
 def app():
     pass
 
+def calculate_distance(row):
+    initial_position = initial_positions.loc[row['Node']]
+    return np.sqrt(
+        (row['x'] - initial_position['x']) ** 2 +
+        (row['y'] - initial_position['y']) ** 2 +
+        (row['z'] - initial_position['z']) ** 2
+    )
+
 dist_df = pd.DataFrame(data=[],columns=['From','To','Distance','timestamp','period'])
 
 dashboard = st.container()
@@ -21,6 +29,7 @@ with st.sidebar:
     Direction = st.selectbox(
         'Select Direction: North-Up, North-Down, South-Up, South-Down',('N-UP', 'N-DN','S-UP','S-DN')
     )
+
 
 datapath = 'data/'
 files = os.listdir(datapath)
@@ -37,7 +46,7 @@ with dashboard:
     st.header(f"Tunnel Convergence Data Visualization")
 
     try:
-        # Plot Graph
+    # Plot Graph
         st.subheader(f"Denchai Chieng Rai - Chieng Khong Project: {contract}-{Direction}")
 
         # Sidebar selection
@@ -47,6 +56,14 @@ with dashboard:
         # Filter DataFrame based on selected timestamps
         filtered_df = df[df['timestamp'].dt.strftime('%Y-%m-%d_%H-%M-%S').isin(selected_timestamps)]
 
+        group_by_node_period = df.groupby(['Node', 'period']).mean().reset_index()
+        group_by_node_period = group_by_node_period.sort_values(by='period')
+        periods = list(set(group_by_node_period['period']))
+        # Find initial position for each Node at period[0]
+        initial_positions = group_by_node_period[group_by_node_period['period'] == periods[0]].set_index('Node')[['x', 'y', 'z']]
+
+        # Calculate distances and add as a new column
+        group_by_node_period['displacement'] = group_by_node_period.apply(calculate_distance, axis=1)
 
         # Plot with Altair
         if not filtered_df.empty:
@@ -69,12 +86,22 @@ with dashboard:
             chart = (points+text).interactive()
 
             st.altair_chart(chart, use_container_width=True)
+
+            # Create the Altair line plot
+            timeline = alt.Chart(group_by_node_period).mark_line().encode(
+                x=alt.X('period:O', title='Period'),
+                y=alt.Y('displacement:Q', title='Distance from Initial Position'),
+                color='Node:N'
+            ).properties(
+                title="Distance from Initial Position Across Periods",
+                width=600,
+                height=400
+            )
+
+            st.altair_chart(timeline, use_container_width=True)
+
         else:
             st.write("No data available for the selected timestamps.")
-
-        # Display filtered DataFrame
-        st.write("Coordinate Datat of Tunnel-" + contract + "-" + Direction)
-        st.write(filtered_df)
 
     except: st.write('Data Not Found!')
 
